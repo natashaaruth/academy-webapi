b171d3d183ba6349c15733a324b8987a5138c99e
albertkelvinhu <albertkelvinhutapea@gmail.com>
2017-08-10 11:50:37 +0700

    get by date diperbaiki

--- common/models/report.js
+++ common/models/report.js
@@ -36,11 +36,9 @@ module.exports = function(Report) {
     Report.getAssignmentsOpenIncludeTask = function(account_id,cb){
         app.models.Assignment.find(
             {
-                where:
+            where:
                 {
                 accountId: account_id,
-                status :"open"
-
             },
             include:{
                 relation: 'task'
@@ -49,6 +47,7 @@ module.exports = function(Report) {
         if(err || account_id === 0)
             return cb(err);
         else {
+            
             cb(null, assignments);
         }
         })
@@ -58,9 +57,9 @@ module.exports = function(Report) {
     Report.remoteMethod("getAssignmentsOpenIncludeTask",
         {
             accepts: [{ arg: 'accountId', type: 'string'}],
-            http: { path:"/account/:account_id/assignments/get/open", verb: "get", errorStatus: 401,},
-            description: ["Mengambil assignments yang berstatus open termasuk task setiap akun"],
-            returns: {arg: "Assignments", type: "object"}
+            http: { path:"/account/:account_id/assignments/", verb: "get", errorStatus: 401,},
+            description: ["Mengambil assignments termasuk task setiap akun"],
+            returns: {arg: "Assignments", type: "array",root:true}
     })
 
     //get close assignment
@@ -104,13 +103,13 @@ module.exports = function(Report) {
         })
     };
 
-  Report.remoteMethod("countAssignment",
+    Report.remoteMethod("countAssignment",
     {
         accepts: [{ arg: 'accountId', type: 'string'}],
         http: { path:"/account/:account_id/assignments/count", verb: "get", errorStatus: 401,},
         description: ["Mengambil jumlah assignments setiap akun"],
         returns: {arg: "count", type: "object",root:true}
-  })
+    })
 
     Report.countClosedAssignmentbyUser = function(account_id,cb){
         app.models.Assignment.count({accountId: account_id,status: "closed"},function(err, count){
@@ -179,7 +178,7 @@ module.exports = function(Report) {
             var sum = assignments.reduce(function(last, d) {
                 return d.budget + last;
             }, 0);
-            cb(null, sum);
+            cb(null, sum*3600);
         }
         }) 
     };
@@ -260,16 +259,19 @@ module.exports = function(Report) {
     Report.getAssignmentsPerDate = function(account_id,date_start,date_end,cb){
         var start_time = new Date(date_start);
         var end_time = new Date(date_end);
-
+        // start_time.setHours(start_time-1);
+        console.log(start_time)
+        end_time.setHours((((end_time.getHours()+(end_time.getTimezoneOffset()/-60))+(23-(end_time.getTimezoneOffset()/-60)))),59,59,0);
         start_time=start_time.toUTCString();
         end_time=end_time.toUTCString();
 
         app.models.Assignment.find (
             {
                 include: 'task',
+                order:'date DESC',
                 where:
                 {accountId: account_id,
-            date:{
+                date:{
                 between: [start_time, end_time]
             }}},
             function(err, assignments){
@@ -285,14 +287,18 @@ module.exports = function(Report) {
         accepts: [{ arg: 'account_id', type: 'string'},{ arg: 'date_start', type: 'string'},{ arg: 'date_end', type: 'string'}],
         http: { path:"/account/:account_id/:date_start/to/:date_end/assignments/", verb: "get", errorStatus: 401,},
         description: ["Total Assignment per akun berdasarkan tanggal."],
-        returns: {arg: "Assignments", type: "array",root: true}
+        returns: {arg: "Assignments", type: "object",root: true}
     })
     
     //kalau dipanggil ke aurelia ga bekerja dengan baik
     Report.getClosedAssignmentsPerDate = function(account_id,date_start,date_end,cb){
         var start_time = new Date(date_start);
+        console.log(start_time)
         var end_time = new Date(date_end);
-
+        start_time.setHours(start_time.getTimezoneOffset());
+        console.log(start_time)
+        end_time.setDate(end_time.getDate+1);
+        end_time.setHours(start_time.getTimezoneOffset());
         start_time=start_time.toUTCString();
         end_time=end_time.toUTCString();
 
@@ -305,9 +311,10 @@ module.exports = function(Report) {
                         {accountId: account_id},
                         {date:{
                             between: [start_time, end_time]}},
-                        {  status: "closed"}
                     ]
-                }
+
+                },
+                order: 'date DESC'
                 },
             function(err, assignments){
         if(err || account_id === 0)
@@ -328,8 +335,12 @@ module.exports = function(Report) {
     //kalau dipanggil ke aurelia ga bekerja dengan baik
     Report.getOpenAssignmentsPerDate = function(account_id,date_start,date_end,cb){
         var start_time = new Date(date_start);
+        console.log(start_time)
         var end_time = new Date(date_end);
-
+        start_time.setHours(start_time.getTimezoneOffset());
+        console.log(start_time)
+        end_time.setDate(end_time.getDate+1);
+        end_time.setHours(start_time.getTimezoneOffset());
         start_time=start_time.toUTCString();
         end_time=end_time.toUTCString();
 
@@ -430,32 +441,30 @@ module.exports = function(Report) {
     })  
 
     Report.getProjectperAccount = function(account_id,cb){
-        var projects=[];
         app.models.Assignment.find({include:"task",scope: {include: "project"},where: {accountId: account_id}},function(err, assignments){
         if(err || account_id === 0)
            return cb(err);
         else {
-            // var promiseProject = [];            
-            // for(var a of assignments){
-            //     var temp = app.models.Project.findOne({where: {id: a.project_id}});
-            //     promiseProject.push(temp);     
-            // } 
-            // Promise.all(promiseProject).then(results => {
-            //     //remove duplicates
-            // var out = [];
-            // for (var i = 0, l = results.length; i < l; i++) {
-            //     var unique = true;
-            //     for (var j = 0, k = out.length; j < k; j++) {
-            //         if (results[i].code === out[j].code) {
-            //             unique = false;
-            //         }
-            //     }
-            //     if (unique) {
-            //         out.push(results[i]);
-            //     }
-            // }
-                cb(null, assignments);
-            // })
+            var promiseProject = [];            
+            for(var a of assignments){
+                var temp = app.models.Project.findOne({where: {id: a.project_id}});
+                promiseProject.push(temp);     
+            } 
+            Promise.all(promiseProject).then(results => {
+            var out = [];
+            for (var i = 0, l = results.length; i < l; i++) {
+                var unique = true;
+                for (var j = 0, k = out.length; j < k; j++) {
+                    if (results[i].code === out[j].code) {
+                        unique = false;
+                    }
+                }
+                if (unique) {
+                    out.push(results[i]);
+                }
+            }
+                cb(null, out);
+            })
         }
         }) 
     };
@@ -508,20 +517,18 @@ module.exports = function(Report) {
     })
 
 //get assignment project account
-    Report.getAssignmentInProject = function(account_id,cb){
+    Report.getAssignmentInProject = function(account_id,project_id,cb){
         app.models.Assignment.find({include:"task",where:{accountId: account_id}},function(err, assignments){
         if(err || account_id === 0)
             return cb(err);
         else {
             var arr = [];
             var newData = {};
-            var a;
-            
-            // for(var a of assignments){   
-            //     if(a.projectId===project_id){
-            //         console.log("ada kok")
-            //         arr.push(a);
-            //     }        
+            for(var a of assignments){   
+                if(a.projectId==project_id){
+                    arr.push(a);
+                }
+            }            
             cb(null,arr);
         }
         }) 
@@ -529,10 +536,10 @@ module.exports = function(Report) {
 
     Report.remoteMethod("getAssignmentInProject",
     {
-        accepts: [{ arg: 'account_id', type: 'string'}],
-        http: { path:"/account/:account_id/assignments/project", verb: "get", errorStatus: 401,},
+        accepts: [{ arg: 'account_id', type: 'string'},{ arg: 'project_id', type: 'string'}],
+        http: { path:"/account/:account_id/:project_id/assignments", verb: "get", errorStatus: 401,},
         description: ["Mengambil assignment yang telah open dari setiap akun."],
-        returns: {arg: "assignments", type: "array",root:true}
+        returns: {arg: "assignments", type: "object",root:true}
     })
 
     Report.getEfficiencyPerProject = function(account_id,project_id,cb){
@@ -550,7 +557,7 @@ module.exports = function(Report) {
             }, 0);
             var efficiency= 0;
             if((sumBudget!=null&&sumBudget!=0) && (sumElapsed!=null&&sumElapsed!=0)){
-                efficiency = ((sumBudget/sumElapsed)*100).toFixed(2);
+                efficiency = (((sumBudget*360)/sumElapsed)*100).toFixed(2);
             }else{
                 efficiency = 0;
             }
